8d76eabbf9b4ea785a1c342d1e7edef650418b9d
Cole Winchester <cowinch@gmail.com>
2022-10-02 16:30:34 -0500

    Path variables complete

---
+++ 01-SpringIntro/assignments/DaikichiPathVariables/DaikichiPathVariables/src/main/java/com/cole/daikichi/controllers/DaikichiController.java
@@ -0,0 +1,33 @@
+package com.cole.daikichi.controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/daikichi")
+public class DaikichiController {
+	@RequestMapping("")
+	public String welcome() {
+		return "Welcome!";
+	}
+	@RequestMapping("/today")
+	public String today() {
+		return "Today you will find luck in all your endeavors!";
+	}
+	@RequestMapping("/tomorrow")
+	public String tomorrow() {
+		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	@RequestMapping("/travel/{destination}")
+	public String travel(@PathVariable("destination") String destination) {
+		return "Contratulations! You will soon travel to "+destination;
+	}
+	@RequestMapping("lotto/{num}")
+	public String lotto(@PathVariable("num") Integer num) {
+		if(num%2==0) {
+			return "You will take a gran journey in the near future, but be weary of tempting offers";
+		}
+		return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends";
+	}
+}
